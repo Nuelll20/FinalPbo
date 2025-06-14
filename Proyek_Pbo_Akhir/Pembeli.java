81efb9fc1969a5b5722d426ae523e6cff3fc635b
Nuelll20 <michaelangelopji20@gmail.com>
2025-06-15 03:30:12 +0700

    Update Pembeli.java

--- Proyek_Pbo_Akhir/Pembeli.java
+++ Proyek_Pbo_Akhir/Pembeli.java
@@ -1,47 +1,35 @@
 public class Pembeli {
     private String nama;
     private String alamat;
-    private Barang barang;
-    private int jumlah;
+    private String noHP;
 
-    public Pembeli() {}
+    public Pembeli(String nama, String alamat, String noHP) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noHP = noHP;
+    }
 
-    public void inputNama(String nama) {
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
         this.nama = nama;
     }
 
-    public void inputAlamat(String alamat) {
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
 
-    public void inputBarang(Barang barang, int jumlah) {
-        this.barang = barang;
-        this.jumlah = jumlah;
+    public String getNoHp() {
+        return noHP;
     }
 
-    public void cetakStruk() {
-        double totalHarga = barang.getHarga() * jumlah;
-        System.out.println("\n===== STRUK PEMBELIAN =====");
-        System.out.println("Nama Pembeli : " + nama);
-        System.out.println("Alamat       : " + alamat);
-        System.out.println("Barang       : " + barang.getNamaBarang());
-        System.out.println("Jumlah       : " + jumlah);
-        System.out.printf("Total Harga  : %.2f\n", totalHarga);
-        System.out.println("===========================");
-    }
-
-    public double getTotalHarga() {
-        return barang.getHarga() * jumlah;
-    }
-
-    public Barang getBarang() {
-        return barang;
-    }
-
-    public int getJumlah() {
-        return jumlah;
-    }
-    public String getNama(){
-        return nama;
+    public void setNoHp(String noHP) {
+        this.noHP = noHP;
     }
 }
